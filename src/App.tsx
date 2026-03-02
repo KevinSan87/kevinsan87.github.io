919eff861685d8704b93c84dfb1962b5f8a56824
[dyad] <kevindesantana87@gmail.com>
2026-03-02 15:24:00 -0300

    [dyad] Implementando HashRouter e caminhos relativos estritos para GitHub Pages - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,13 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* Removido o basename fixo para funcionar tanto no root quanto em subpastas */}
-      <BrowserRouter>
+      {/* HashRouter é ideal para GitHub Pages pois não depende de configuração de servidor para rotas */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
